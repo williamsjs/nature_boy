d9d5f8331e6af45ae4305605738d76f51ef78457
williamsjs <williamsjs1224@gmail.com>
2017-01-29 21:01:29 -0500

    add main categories to login page

--- app/controllers/categories/category_controller.rb
+++ app/controllers/categories/category_controller.rb
@@ -1,7 +1,6 @@
 class Category < ApplicationController
 
   def show
-    @category = Category.find(params[:id])
   end
 
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,11 @@
 class Users::SessionsController < Devise::SessionsController
 # before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  #GET /resource/sign_in
+  def new
+    @categories = Category.all
+    super
+  end
 
   # POST /resource/sign_in
   # def create
